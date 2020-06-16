28aa60a203f33b7502264723ed5e656c3da2ed64
Mathieu-Be <Mathieu-Be@users.noreply.github.com>
2020-06-16 17:21:30 +0200

    --- Initial commit

---
+++ js/cuda.js
@@ -0,0 +1,21 @@
+// Menus 
+$('a[class^="nav"]').click(function () {
+    var $class = $(this).parents('ul').attr('class');
+    console.log('ul[class="' + $class + '"] a[class^="nav"]');
+    $('ul[class="' + $class + '"] a[class^="nav"]').removeClass('active');
+    $(this).addClass('active');
+});
+
+// 
+
+$('.activities li').click(function () {
+    $('.activities li').removeClass('active');
+    $(this).addClass('active');
+    var $target = $(this).attr('data-type');
+    $('.desktop div').hide();
+    if ($target == 'all'){
+        $('.desktop div').show();
+    } else {
+        $('.desktop div[data-target~="' + $target + '"]').show();
+    }
+});
